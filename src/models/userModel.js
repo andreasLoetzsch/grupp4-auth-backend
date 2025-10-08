ff3b3217b68d799d183695031f42833dacde59be
andreasLoetzsch <168843741+andreasLoetzsch@users.noreply.github.com>
2025-10-08 21:51:46 +0200

    feat: added userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userModel = new Schema({
+    username: {type: string, required: true  },
+    password: {type: string, required: true},
+    email: {type: string, required: true},
+    roel: {type: string},
+    phoneNumber: {type: number} 
+})
+
+userModel.pre("save", async function (next) {
+    if(!this.isModified(password))return next();
+    const genSalt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, genSalt);
+    next()
+})
+
+module.exports = mongoose.model('user', userModel)
