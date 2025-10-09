4fccaeb0244b7f51b5f4f30166ad274c02163adf
andreasLoetzsch <168843741+andreasLoetzsch@users.noreply.github.com>
2025-10-09 13:17:30 +0200

    fixed: hash bugg in userSchema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs')
 
 const { Schema } = mongoose;
 
-const userModel = new Schema({
+const userSchema = new Schema({
     username: {type: String, required: true  },
     password: {type: String, required: true},
     email: {type: String, required: true},
@@ -11,15 +11,15 @@ const userModel = new Schema({
     phoneNumber: {type: Number} 
 })
 
-userModel.pre("save", async function (next) {
-    if(!this.isModified(password))return next();
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password"))return next();
     const genSalt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, genSalt);
     next()
 })
 
-userModel.methods.checkPassword = function(password) {
+userSchema.methods.checkPassword = function(password) {
     return bcrypt.compare(password, this.password)
 }
 
-module.exports = mongoose.model('user', userModel)
+module.exports = mongoose.model('User', userSchema)
