459e7af2f64270a99444b44c8f85909d779762d7
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-10-10 09:04:57 +0200

    added helmet middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const authRouter = require('./routes/authRoutes')
 const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 
 const app = express()
 
+app.use(helmet());
 app.use(express.json())
 app.use(cookieParser());
 
