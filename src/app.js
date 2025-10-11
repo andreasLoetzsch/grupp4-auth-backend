a38ad6ef8f48c05dde269dd7328a10690f39dee2
angelika-friis <angelikafriis@gmail.com>
2025-10-11 16:17:17 +0200

    feat: added CORS policy

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ const express = require('express')
 const authRouter = require('./routes/authRoutes')
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express()
 
 app.use(helmet());
 app.use(express.json())
 app.use(cookieParser());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use('/auth', authRouter)
 
