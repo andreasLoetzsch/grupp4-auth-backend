5b2804c532b16fe090bd726e45a6888ce35095b7
Hampus von Horn <hampus.vh@gmail.com>
2025-10-12 12:33:43 +0200

    add logoutUser route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const {registerUser, loginUser, deleteUser} = require('../controllers/authController')
+const {registerUser, loginUser, logoutUser, deleteUser} = require('../controllers/authController')
 
 const authRouter = express.Router()
 
 authRouter.post('/register', registerUser)
 authRouter.post('/login', loginUser)
+authRouter.post("/logout", logoutUser)
 authRouter.delete('/delete', deleteUser)
 
 module.exports = authRouter
