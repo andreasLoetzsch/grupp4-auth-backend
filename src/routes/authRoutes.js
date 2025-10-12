4feb6162059098710ce6da779bc2b82d44537f98
Hampus von Horn <hampus.vh@gmail.com>
2025-10-12 16:29:29 +0200

    add updateUser route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const {registerUser, loginUser, logoutUser, deleteUser} = require('../controllers/authController')
+const { registerUser, loginUser, logoutUser, updateUser, deleteUser } = require('../controllers/authController')
 
 const authRouter = express.Router()
 
 authRouter.post('/register', registerUser)
 authRouter.post('/login', loginUser)
 authRouter.post("/logout", logoutUser)
+authRouter.patch('/update/:id', updateUser)
 authRouter.delete('/delete', deleteUser)
 
 module.exports = authRouter
