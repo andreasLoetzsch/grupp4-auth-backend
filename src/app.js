7993bf58d0bfb469d80be4bd1f517d6642363e30
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-10-14 16:21:08 +0200

    added sanitizing for mongodb and express

--- src/app.js
+++ src/app.js
@@ -4,13 +4,35 @@ const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 const cors = require("cors");
 const swaggerUi = require('swagger-ui-express')
-const swaggerSpec = require('./config/swagger')
+const swaggerSpec = require('./config/swagger');
+const bodyParser = require('body-parser');
 
 const app = express()
 
 app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json())
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  const sanitize = (obj) => {
+    if(obj && typeof obj == "object") {
+      for(const key in obj) {
+        if(key.startsWith("$") || key.includes(".")) {
+          delete obj[key];
+        } else {
+          sanitize(obj[key]);
+        }
+      }
+    }
+  }
+
+  sanitize(req.body);
+  sanitize(req.query);
+  sanitize(req.params);
+  next();
+});
 
 app.use(cors({
   origin: "http://localhost:5173",
